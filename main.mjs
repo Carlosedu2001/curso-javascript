6d4a8c7741799c3fa3719e8664e66489cd80adcf
Carlosedu2001 <kdu-lacerda@hotmail.com>
2022-07-11 19:54:16 -0300

    Não está funcionando

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraCidade, mostraHobby} from './funcoes.mjs';
+
+console.log(mostraIdade("Carlos", 26));
+console.log(mostraCidade("Carlos", "Sorocaba"));
+console.log(mostraHobby("Carlos", "Tocar piano / Jogar"));
