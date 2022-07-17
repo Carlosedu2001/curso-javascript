d37d4344fdee1731e716e65564868961b5b1ad47
Carlosedu2001 <kdu-lacerda@hotmail.com>
2022-07-17 20:25:14 -0300

    Modo escuro e modo claro

---
+++ scripts-manipulando-dom.js
@@ -0,0 +1,30 @@
+function changeMode() {
+	changeClasses();
+	changeText();
+}
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+}
+
+function changeText() {
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+
+	if (body.classList.contains(darkModeClass)) {
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ': ON';
+		return;
+	}
+
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ': ON';
+}
+
+const darkModeClass = 'dark_mode';
+const button = document.getElementById('mode_selector');
+const h1 = document.getElementById('page_title');
+const body = document.getElementsByTagName('body')[0];
+button.addEventListener('click', changeMode);
